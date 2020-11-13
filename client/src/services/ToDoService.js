fca01568b4181dbf7a83e334ed9c0c7cb3ddefe1
Duane Haworth <duanehaworth@hotmail.com>
2020-11-13 17:21:18 -0600

    Vue talking to Python

---
+++ client/src/services/ToDoService.js
@@ -0,0 +1,20 @@
+// https://stackoverflow.com/questions/41164672/whats-the-equivalent-of-angular-service-in-vuejs/54165192#54165192
+
+import axios from 'axios';
+
+let baseUrl = "http://localhost:5000";
+let axiosInstance = null;
+
+export default new class {
+    
+    constructor(){
+        axiosInstance = axios.create({
+            baseURL: baseUrl,            
+            headers: { 'Content-Type': 'application/json' }  
+        });
+    }
+
+    getPong(){
+        return axiosInstance("/ping");
+    }
+ }
--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -5,12 +5,29 @@
 </template>
 
 <script>
+import todoService from '@/services/ToDoService'
+
 export default {
   name: 'Ping',
   data() {
     return {
-      msg: 'Hello!',
+      msg: 'TEST',
     };
+  },
+  methods: {
+    getMessage() {
+      todoService.getPong()
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
   },
 };
 </script>
